efef4bf7fe8bb0cb7a20467b24f435911f286c1a
Zeddyare <w0433704@nscc.ca>
2026-02-18 09:47:05 -0400

    Commit Demo Code for update

--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import logo from '../assets/FANSlogo.png';
+import MapComponent from '../map.jsx';
 
 export default function HomePage({ onGetStarted }) {
   const navigate = useNavigate();
